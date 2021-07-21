20e3cab29ddb37ff5190879d6532a5d5f2a51d7a
KILLER <78667474+alexander695@users.noreply.github.com>
2021-07-21 09:55:48 -0700

    Add files via upload

---
+++ Prank.py
@@ -0,0 +1,58 @@
+print '\033[1;91m'+'''FACEBOOK HACK'''+'\033[0;0m'
+
+
+
+import time
+import os
+import webbrowser
+nmr = raw_input("what victim's Name?:")
+print '\033[31m'+'victim Name is: ?'+'\033[0;0m', nmr
+s = raw_input("yes or no?: ")
+time.sleep(3) # Sleep for 1 seconds
+print ""
+print '\033[31m'+'reading backup file and message from'+'\033[0;0m', nmr
+print ""
+time.sleep(2) # Sleep for 1 seconds
+print '\033[1;92m'+'20%'+'\033[0;0m'
+print ""
+time.sleep(3) # Sleep for 1 seconds
+print '\033[1;92m'+'40%'+'\033[0;0m'
+print ""
+time.sleep(1) # Sleep for 1 seconds
+print '\033[1;92m'+'70%'+'\033[0;0m'
+print ""
+time.sleep(1) # Sleep for 1 seconds
+print '\033[1;92m'+'90%'+'\033[0;0m'
+print ""
+time.sleep(1) # Sleep for 1 seconds
+print '\033[1;92m'+'100%'+'\033[0;0m'
+print ""
+time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Facebook photos...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
+print ""
+time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Facebook audio...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
+print ""
+time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Facebook stickers...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
+print time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'hacking database...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print '\033[1;94m'+'Found!...'+'\033[0;0m'time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Password...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'time.sleep(4) # Sleep for 1 seconds
+print '\033[1;92m'+'Email...'+'\033[0;0m'
+time.sleep(2) # Sleep for 1 seconds
+print ""
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'

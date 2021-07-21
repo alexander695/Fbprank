f27f673da78ebadecdb4a917fb94f81b4fb07888
KILLER <78667474+alexander695@users.noreply.github.com>
2021-07-21 10:00:49 -0700

    Update Prank.py

--- Prank.py
+++ Prank.py
@@ -14,45 +14,36 @@ print '\033[31m'+'reading backup file and message from'+'\033[0;0m', nmr
 print ""
 time.sleep(2) # Sleep for 1 seconds
 print '\033[1;92m'+'20%'+'\033[0;0m'
-print ""
 time.sleep(3) # Sleep for 1 seconds
 print '\033[1;92m'+'40%'+'\033[0;0m'
-print ""
 time.sleep(1) # Sleep for 1 seconds
 print '\033[1;92m'+'70%'+'\033[0;0m'
-print ""
 time.sleep(1) # Sleep for 1 seconds
 print '\033[1;92m'+'90%'+'\033[0;0m'
-print ""
 time.sleep(1) # Sleep for 1 seconds
 print '\033[1;92m'+'100%'+'\033[0;0m'
-print ""
 time.sleep(4) # Sleep for 1 seconds
 print '\033[1;92m'+'Facebook photos...'+'\033[0;0m'
 time.sleep(2) # Sleep for 1 seconds
-print ""
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
-print ""
 time.sleep(4) # Sleep for 1 seconds
 print '\033[1;92m'+'Facebook audio...'+'\033[0;0m'
 time.sleep(2) # Sleep for 1 seconds
-print ""
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
-print ""
-time.sleep(4) # Sleep for 1 seconds
+time.sleep(1) # Sleep for 1 seconds
 print '\033[1;92m'+'Facebook stickers...'+'\033[0;0m'
-time.sleep(2) # Sleep for 1 seconds
-print ""
+time.sleep(1) # Sleep for 1 seconds
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
-print time.sleep(4) # Sleep for 1 seconds
+time.sleep(1) # Sleep for 1 seconds
 print '\033[1;92m'+'hacking database...'+'\033[0;0m'
 time.sleep(2) # Sleep for 1 seconds
-print '\033[1;94m'+'Found!...'+'\033[0;0m'time.sleep(4) # Sleep for 1 seconds
+print '\033[1;94m'+'Found!...'+'\033[0;0m'time.sleep(4) 
+time.sleep(1)# Sleep for 1 seconds
 print '\033[1;92m'+'Password...'+'\033[0;0m'
 time.sleep(2) # Sleep for 1 seconds
-print ""
-print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'time.sleep(4) # Sleep for 1 seconds
+print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'time.sleep(4)
+# Sleep for 1 seconds
 print '\033[1;92m'+'Email...'+'\033[0;0m'
 time.sleep(2) # Sleep for 1 seconds
-print ""
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
+print 'the password and email was saved in disk :D'

521342eccbc16fe17789ffca24916457b0217dbd
KILLER <78667474+alexander695@users.noreply.github.com>
2021-07-22 09:34:20 -0700

    Update Prank.py

--- Prank.py
+++ Prank.py
@@ -4,7 +4,7 @@ print 'FACEBOOK HACK'.center(50)
 
 import time
 import os
-import Chrome
+import webbrowser,Chrome
 nmr = raw_input("what victim's Name?:")
 print '\033[31m'+'victim Name is: ?'+'\033[0;0m', nmr
 s = raw_input("yes or no?: ")
@@ -21,7 +21,7 @@ print '\033[1;92m'+'90%'+'\033[0;0m'
 time.sleep(2) # Sleep for 2 seconds
 print '\033[1;92m'+'100%'+'\033[0;0m'
 time.sleep(3) # Sleep for 4 seconds
-print '\033[1;92m'+'Facebok photos...'+'\033[0;0m'
+print '\033[1;92m'+'Facebook photos...'+'\033[0;0m'
 time.sleep(2) # Sleep for 2 seconds
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
 time.sleep(3) # Sleep for 4 seconds
@@ -47,4 +47,5 @@ print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
 print '0\33[1;92m'+'the password and email was saved in disk :D'+'\033[0;0m'
 print '0\33[1;92m'+'you are begin redirected to facebook'+'\033[0;0m'
 time.sleep(42)
-Chrome.open('https://xvideos.com')
+def open
+Chrome.open('https://facebook.com')

c758ba195b959f6a924395f17400dac874345378
KILLER <78667474+alexander695@users.noreply.github.com>
2021-08-08 11:04:59 -0700

    Update Prank.py

--- Prank.py
+++ Prank.py
@@ -21,17 +21,17 @@ time.sleep(2)
 print '\033[1;92m'+'20%'+'\033[0;0m'
 time.sleep(3) # Sleep for 3 seconds
 print '\033[1;92m'+'40%'+'\033[0;0m'
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(3) # Sleep for 3 seconds
 print '\033[1;92m'+'70%'+'\033[0;0m'
-time.sleep(3) # Sleep for 2 seconds
+time.sleep(3) # Sleep for 3 seconds
 print '\033[1;92m'+'90%'+'\033[0;0m'
 time.sleep(2) # Sleep for 2 seconds
 print '\033[1;92m'+'100%'+'\033[0;0m'
-time.sleep(3) # Sleep for 4 seconds
+time.sleep(3) # Sleep for 3 seconds
 print '\033[1;92m'+'Facebook photos...'+'\033[0;0m'
 time.sleep(2) # Sleep for 2 seconds
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
-time.sleep(3) # Sleep for 4 seconds
+time.sleep(3) # Sleep for 3 seconds
 print '\033[1;92m'+'Facebook audio...'+'\033[0;0m'
 time.sleep(2) # Sleep for 2 seconds
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
@@ -39,17 +39,17 @@ time.sleep(2) # Sleep for 2 seconds
 print '\033[1;92m'+'Facebook stickers...'+'\033[0;0m'
 time.sleep(2) # Sleep for 2 seconds
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(4) # Sleep for 4 seconds
 print '\033[1;92m'+'hacking database...'+'\033[0;0m'
 time.sleep(2) # Sleep for 2 seconds
 print '\033[1;94m'+'Hacked!...'+'\033[0;0m'
-time.sleep(2)# Sleep for 2 seconds
+time.sleep(3)# Sleep for 3 seconds
 print '\033[1;92m'+'Password...'+'\033[0;0m'
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(3) # Sleep for 3 seconds
 print '\033[1;94m'+'downloading...'+'\033[0;0m'
 
 print '\033[1;92m'+'Email...'+'\033[0;0m'
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(3) # Sleep for 3 seconds
 print '\033[1;94m'+'Found! downloading...'+'\033[0;0m'
 print '\33[1;92m'+'the Data was saved in disk :D'+'\033[0;0m'
 
